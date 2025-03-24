592a674e8e369d6d2cc689d57f134e03aa3d1225
Gustavo Aquino <gustavo.aquino.dev@gmail.com>
2025-03-23 21:33:04 -0300

    feat: add grid layout to dashboard

--- app.vue
+++ app.vue
@@ -1,35 +1,74 @@
 <template>
-    <v-row style="height: 100vh">
-      <v-col class="d-flex flex-column justify-center align-center">
-        <v-btn
-          v-for="(button, index) in buttons"
-          :key="index"
-          class="ma-2"
-          :color="button.color"
-        >
-          {{ button.label }}
-        </v-btn>
-      </v-col>
-    </v-row>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        buttons: [
-          { label: 'Vue', color: 'red' },
-          { label: 'Vue Router', color: 'green' },
-          { label: 'Vuex', color: 'yellow' },
-          { label: 'Vue I18n', color: 'purple' },
-          { label: 'Vue Composition API', color: 'orange' },
-          { label: 'Vue Devtools', color: 'pink' },
-          { label: 'Vue CLI', color: 'teal' },
-          { label: 'Vue Test Utils', color: 'cyan' },
-          { label: 'Vue SSR', color: 'indigo' },
-          { label: 'Vuetify', color: 'blue' },
-        ],
-      };
-    },
-  };
-  </script>
+  <v-app>
+    <v-app-bar class=" text-center bord-b">
+      <v-app-bar-title>Meu app</v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <h1>Dashboard</h1>
+        <v-row>
+          <v-col cols="12" md="4" >
+            <v-card flat class="border mx-auto" max-width="300" >
+              <v-img
+                src="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                class="align-end text-white">
+                <v-card-title>
+                  Melhores frameworks
+                </v-card-title>
+              </v-img>
+              <v-card-subtitle class="text-subtitle-1 pt-3">Vue.js</v-card-subtitle>
+              <v-card-text class="text-black">
+                <div>
+                Vue é um dos frameworks JavaScript mais populares, projetado para ser simples de aprender e poderoso o suficiente para criar aplicações complexas. Se você busca otimizar a criação de interfaces dinâmicas e interativas, Vue é a escolha ideal.
+                </div>
+              </v-card-text>
+              <v-card-actions>
+              <v-btn class="font-weight-bold">Ver mais</v-btn>
+            </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col cols="12" md="4">
+            <v-card flat class="border mx-auto" max-width="300" >
+              <v-img
+                src="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                class="align-end text-white">
+                <v-card-title>
+                  Melhores frameworks
+                </v-card-title>
+              </v-img>
+              <v-card-subtitle class="text-subtitle-1 pt-3">Vue.js</v-card-subtitle>
+              <v-card-text class="text-black">
+                <div>
+                Vue é um dos frameworks JavaScript mais populares, projetado para ser simples de aprender e poderoso o suficiente para criar aplicações complexas. Se você busca otimizar a criação de interfaces dinâmicas e interativas, Vue é a escolha ideal.
+                </div>
+              </v-card-text>
+              <v-card-actions>
+              <v-btn class="font-weight-bold">Ver mais</v-btn>
+            </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col cols="12" md="4">
+            <v-card flat class="border mx-auto" max-width="300">
+              <v-img
+                src="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                class="align-end text-white">
+                <v-card-title>
+                  Melhores frameworks
+                </v-card-title>
+              </v-img>
+              <v-card-subtitle class="text-subtitle-1 pt-3">Vue.js</v-card-subtitle>
+              <v-card-text class="text-black">
+                <div>
+                Vue é um dos frameworks JavaScript mais populares, projetado para ser simples de aprender e poderoso o suficiente para criar aplicações complexas. Se você busca otimizar a criação de interfaces dinâmicas e interativas, Vue é a escolha ideal.
+                </div>
+              </v-card-text>
+              <v-card-actions>
+              <v-btn class="font-weight-bold">Ver mais</v-btn>
+            </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
